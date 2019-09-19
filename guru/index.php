e91ddcaffea277741105ad45c4d041c6856d671d
jurnalmuis <fauco.mail@gmail.com>
2019-09-19 13:35:36 +0700

    Tampilan

---
+++ guru/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login Teacher CBT</title>
+    <link rel="shortcut icon" href="../img/test.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/edit.css">
+    <script src="../js/bootstrap.min.js"></script>
+    <style>
+    .gambar{
+      background-image: url('../img/sunset.jpg');
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+      background-position: center;
+    }
+    </style>
+  </head>
+  <body class="gambar">
+    <div class="container-fluid text-center">
+      <form class="login-margin">
+        <div class="">
+          <img src="../img/teacher.png" style="width:150px;">
+        </div>
+        <div class="text-light font-weight-bold">
+          Teacher CBT
+        </div>
+        <div class="mt-1">
+          <input type="text" class="rounded-0" style="width:250px; padding:7px;" placeholder="Masukkan Username">
+        </div>
+        <div class="mt-1">
+          <input type="password" class="rounded-0" style="width:250px; padding:7px;" placeholder="Masukkan Password">
+        </div>
+        <div class="mt-4">
+          <button type="submit" class="btn btn-primary rounded-0" style="width:250px;"><strong>MASUK</strong></button>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
---
+++ siswa/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Beranda</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  </head>
+  <body>
+    <!-- Navbar Menu Start -->
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand mb-1 mt-1 h1" href="#">Beranda</a>
+    </nav>
+    <!-- Navbar Menu End -->
